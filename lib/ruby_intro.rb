7f16f44ad9ebae5e559640a269aa07908c2d6efd
ShaanSinghGill <70672303+ShaanSinghGill@users.noreply.github.com>
2021-09-01 00:38:11 -0700

    Finished
    CHIPS 2.5

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,33 +3,105 @@
 # Part 1
 
 def sum arr
-  # YOUR CODE HERE
+  sum = 0
+  arr.each {|a| sum += a}
+  sum
 end
 
 def max_2_sum arr
-  # YOUR CODE HERE
+  initial = 0
+  if arr.length == 0 
+    return 0
+  elsif arr.length == 1 
+    return arr.max
+  else
+    largest = arr.max
+    index_of_largest = arr.rindex(largest)
+    arr.delete_at(index_of_largest)
+    slargest = arr.max
+    return largest+ slargest
+  end
 end
 
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+  past = []
+  arr.each {|a| 
+    if past.include? n-a
+      return true
+    end
+    past.push(a)
+    }
+    return false
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  name= "Hello, " + name
+  return name
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+  if s.length  == 0 || !s.is_a?(String)
+    return false
+  end 
+
+  vowels = ["a", "e", "i" ,"o" ,"u"]
+  first_letter_of_s = s[0].downcase
+
+  if !first_letter_of_s.match?(/[[:alpha:]]/) 
+    return false
+  end 
+
+  if !vowels.include? first_letter_of_s
+    return true
+  end
+
+  return false
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+  if s.length == 0 
+    return false 
+  end
+  if s.chars.all? {|x| x =~ /[01]/} 
+    value = s.to_i(2)
+    if value % 4 == 0 
+      return true 
+    end 
+  end 
+  return false
 end
 
 # Part 3
 
 class BookInStock
-# YOUR CODE HERE
-end
+  @isbn
+  @price
+  def initialize(isbn, price)
+    if !isbn.is_a?(String) || isbn.length == 0
+      raise ArgumentError
+    end 
+    if price <=  0
+      raise ArgumentError
+    end 
+    @isbn = isbn
+    @price = price
+  end 
+  def isbn
+    return @isbn
+  end 
+  def price
+    return @price 
+  end 
+
+  def isbn=(isbn)
+    @isbn = isbn
+  end 
+  def price= (price)
+    @price = price
+  end 
+  def price_as_string 
+    sprintf("$%2.2f", @price)
+  end
+ end
